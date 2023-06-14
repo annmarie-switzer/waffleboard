24c7414fad763218fc87b01f5d5c7e0d6bfe53fa
Annmarie Switzer <aargiros@gmail.com>
2023-06-14 19:41:57 -0400

    remove articles

--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_08_021814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "players", force: :cascade do |t|
     t.string "nhl_id"
     t.string "status"
