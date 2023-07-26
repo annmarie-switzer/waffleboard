24279c4c8e3d1fafe3b6e072f7d07c00cf3d1a39
Annmarie Switzer <aargiros@gmail.com>
2023-07-25 22:29:11 -0400

    new endpoints

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,11 @@ class GamesController < ApplicationController
     game = Game.find(params[:id])
     render json: game
   end
+
+  def players
+    game = Game.find(params[:id])
+    home_players = game.home_team.players
+    away_players = game.away_team.players
+    render json: { home: home_players, away: away_players }
+  end
 end
