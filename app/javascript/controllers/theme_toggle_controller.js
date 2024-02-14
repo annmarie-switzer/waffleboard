4c3bca5f34ea48d0d39a4a59e8971d4266e816f5
Annmarie Switzer <aargiros@gmail.com>
2024-02-13 21:55:45 -0500

    add themes and toggle

---
+++ app/javascript/controllers/theme_toggle_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['icon'];
+
+  static values = {
+    theme: String
+  };
+
+  connect() {
+    this.themeValue = localStorage.getItem('theme') || 'dark';
+    this.changeTheme();
+  }
+
+  toggle() {
+    this.themeValue = this.themeValue === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', this.themeValue);
+    this.changeTheme();
+  }
+
+  changeTheme() {
+    document.body.setAttribute('data-theme', this.themeValue);
+
+    const iconSrc =
+      this.themeValue === 'light' ? 'light-icon.png' : 'dark-icon.png';
+
+    this.iconTarget.src = iconSrc;
+  }
+}
